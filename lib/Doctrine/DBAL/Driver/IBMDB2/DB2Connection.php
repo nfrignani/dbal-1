1b12908c5087fb80a94ae3da56a368e6f16c3090
Nicola Frignani <nicola.frignani@gmail.com>
2015-09-28 15:44:24 +0200

    Update DB2Connection.php

--- lib/Doctrine/DBAL/Driver/IBMDB2/DB2Connection.php
+++ lib/Doctrine/DBAL/Driver/IBMDB2/DB2Connection.php
@@ -100,21 +100,15 @@ class DB2Connection implements Connection, ServerInfoAwareConnection
      */
     public function quote($input, $type=\PDO::PARAM_STR)
     {
-    /* NFRIGNANI 20150928
+    
         $input = db2_escape_string($input);
         if ($type == \PDO::PARAM_INT) {
             return $input;
         } else {
+                $input = str_replace("''", "'", $input); /* NFRIGNANI 20150928 */ 
             return "'".$input."'";
         }
-    */
-        $input = db2_escape_string($input);
-        if ($type == \PDO::PARAM_INT) {
-            return $input;
-        } else {
-        $input = str_replace("''", "'", $input);
-            return "'".$input."'";
-        }
+    
 //        return "'" . addcslashes($input, "\000\n\r\\\032") . "'";
     }
 
